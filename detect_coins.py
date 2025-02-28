6d5ebceba6b99c335fde16fc8f2e2b447f00d6dc
Özgün <121379149+ozgun-ozdemir@users.noreply.github.com>
2025-03-01 00:14:27 +0300

    Add files via upload

--- detect_coins.py
+++ detect_coins.py
@@ -1,25 +1,27 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-image = cv2.imread('coins.png')
+image = cv2.imread('documents/Computer-Vision-Projects-main/images/coins.jpeg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-blurred = cv2.GaussianBlur(gray, (11, 11), 0)
-
-circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1.2, minDist=90,
-                            param1=50, param2=30, minRadius=40, maxRadius=80)
+# Apply Gaussian blur 
+blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+# Detect circles using Hough Transform
+circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1.2, minDist=120,
+                            param1=100, param2=100, minRadius=70, maxRadius=180)
 
 num_coins = 0
 if circles is not None:
     circles = np.round(circles[0, :]).astype("int")
     num_coins = len(circles)
     for (x, y, r) in circles:
-        cv2.circle(image, (x, y), r, (0, 255, 0), 4)
-        cv2.rectangle(image, (x - 10, y - 10), (x + 10, y + 10), (0, 128, 255), -1)
+        cv2.circle(image, (x, y), r, (0, 255, 0), 5) # Draw the circle in green
+        cv2.rectangle(image, (x - 10, y - 10), (x + 10, y + 10), (0, 128, 255), -1)  #Draw an orange rectangle at the center of the circle
 
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-cv2.putText(image, f"Coins: {num_coins}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-cv2.imshow("Coins Detection", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+plt.imshow(image_rgb)
+plt.title(f"Coins Detected: {num_coins}")
+plt.axis('off')  
+plt.show()
